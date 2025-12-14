9dcea75d307ce24206184655b3069197a78b7e15
t_coding <trapperwetzel@gmail.com>
2025-12-14 16:30:16 -0600

    start of tanstack router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 export default defineConfig({
-  plugins: [react()],
+
+  plugins: [tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true,
+  }), react(),],
 })
