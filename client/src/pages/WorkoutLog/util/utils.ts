b82d9394a87c80e2a06bdefd894373dcf21cff5b
t_coding <trapperwetzel@gmail.com>
2025-12-22 23:39:17 -0600

    start utils

---
+++ client/src/pages/WorkoutLog/util/utils.ts
@@ -0,0 +1,12 @@
+import type { WorkoutLogRow } from "../types/types.ts";
+
+
+//combine reps and sets into one field so we can use it inside the table.
+
+interface repsAndSets {
+
+}
+const combineRepsAndSets = (sets: number, reps: number) => {
+
+
+}
